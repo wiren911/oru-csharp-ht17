f4bbda372794aa258f7a2cc919105b10c3a321ec
johannordin <johan.nordin@outlook.com>
2017-09-28 15:31:25 +0200

    calculator-shell

---
+++ F2 Excercise 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F2_Excercise_1
+{
+    class Program
+    {
+        /// <summary>
+        /// Skapa en enkel kalkylator
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! Let me calculate something.");
+            
+        }
+    }
+}
