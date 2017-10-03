750427b041434acb0eca66b6de9f45e3d848f8cf
johannordin <johan.nordin@outlook.com>
2017-10-03 14:03:38 +0200

    exceptiontests

--- Tests/F3/F_Exceptions.cs
+++ Tests/F3/F_Exceptions.cs
@@ -10,14 +10,44 @@ public class ExceptionTests
         throw new Exception("Woops.");
     }
 
-    [TestMethod, ExpectedException(typeof(NotImplementedException))]
+    [TestMethod, ExpectedException(typeof(Exception))]
     public void catch_exception()
     {
         var value = "";
+        var divideBy = 0;
         try
         {
             value += "before ";
-            throw_exception();
+            var result = 1 / divideBy;
+            value += "after ";
+        }
+        catch (DivideByZeroException e)
+        {
+            // AHA! "Förväntat" fel
+            throw new Exception("Du kan inte dela med noll!");
+        }
+        catch (Exception e)
+        {
+            value += "catch ";
+            throw new NotImplementedException("Something is rotten...");
+        }
+        finally
+        {
+            value += "finally ";
+        }
+
+        Assert.AreEqual("before catch finally ", value);
+    }
+
+    [TestMethod]
+    public void no_exception()
+    {
+        var value = "";
+        var divideBy = 1;
+        try
+        {
+            value += "before ";
+            var result = 1 / divideBy;
             value += "after ";
         }
         catch (Exception e)
@@ -32,6 +62,6 @@ public class ExceptionTests
             value += "finally ";
         }
 
-        Assert.AreEqual("before after catch finally ", value);
+        Assert.AreEqual("before after finally ", value);
     }
 }
