f71ce9c1bd7b92cf8173cf7240ec459eaa46a028
johannordin <johan.nordin@outlook.com>
2017-10-04 15:47:49 +0200

    F3 Game 5

--- F3Game/Program.cs
+++ F3Game/Program.cs
@@ -17,6 +17,38 @@ namespace F3Game
 
 
             PrintGrid(a, x, y);
+
+            while (sum < 10)
+            {
+                var move = Console.ReadKey();
+                switch (move.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (y > 0)
+                            y--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (y < a.GetLength(0))
+                            y++;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        if (x > 0)
+                            x--;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (x < a.GetLength(1))
+                            x++;
+                        break;
+                    default:
+                        break;
+                }
+
+                sum += a[y, x];
+                a[y, x] = 0;
+                Console.Clear();
+                PrintGrid(a, x, y);
+                Console.WriteLine("Sum: " + sum);
+            }
         }
 
         private static int[,] GenerateArray()
