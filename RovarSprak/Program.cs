0a713cad6f2f236a17734fe133b332e97b6ba5bb
johannordin <johan.nordin@outlook.com>
2017-10-04 11:37:25 +0200

    RovarSprak

---
+++ RovarSprak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RovarSprak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var vocals = new[] { 'a', 'e', 'i', 'o', 'u', 'y', 'å', 'ä', 'ö' };
+
+            var output = "";
+            foreach (char c in input)
+            {
+                if (vocals.Contains(c) == false)
+                    output += $"{c}o{c}";                
+            }
+
+            Console.WriteLine(output);
+            Console.ReadKey();
+        }
+    }
+}
