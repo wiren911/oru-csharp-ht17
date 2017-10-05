cfc0c11a8309a6f678d411a1db1ae977dd93354d
johannordin <johan.nordin@outlook.com>
2017-10-05 07:16:24 +0200

    F2 Prime number

--- Primtal/Program.cs
+++ Primtal/Program.cs
@@ -10,7 +10,26 @@ namespace Primtal
     {
         static void Main(string[] args)
         {
+            var max = int.Parse(Console.ReadLine());
 
+            for (decimal i = 2; i < max; i++)
+            {
+                var isPrime = true;
+                for (decimal j = 2; j < i; j++)
+                {
+                    var rest = i % j;
+                    if (rest == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime == true)
+                    Console.WriteLine(i + " är ett primtal");
+            }
+
+            Console.ReadKey();
         }
     }
 }
