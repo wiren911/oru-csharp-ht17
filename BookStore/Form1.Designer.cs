ab7953c678b75d48937b1fb694b6e0a1602c7463
johannordin <johan.nordin@outlook.com>
2017-10-06 09:37:33 +0200

    Pairprogramming 2: Bookstore 1a

---
+++ BookStore/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace BookStore
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.numInStock = new System.Windows.Forms.NumericUpDown();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numInStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(12, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 20);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(12, 38);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(100, 20);
+            this.txtAuthor.TabIndex = 1;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(12, 64);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(47, 20);
+            this.txtPrice.TabIndex = 2;
+            // 
+            // numInStock
+            // 
+            this.numInStock.Location = new System.Drawing.Point(65, 64);
+            this.numInStock.Name = "numInStock";
+            this.numInStock.Size = new System.Drawing.Size(47, 20);
+            this.numInStock.TabIndex = 3;
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(12, 90);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(100, 23);
+            this.btnAddBook.TabIndex = 4;
+            this.btnAddBook.Text = "Add";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAdd_click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 346);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.numInStock);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numInStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.NumericUpDown numInStock;
+        private System.Windows.Forms.Button btnAddBook;
+    }
+}
+
---
+++ BookStore/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookStore
+{
+    struct Book
+    {
+        public string Title;
+        public string Author;
+        public decimal Price;
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_click(object sender, EventArgs e)
+        {
+            var title = txtTitle.Text;
+            var author = txtAuthor.Text;
+            var isPrice = decimal.TryParse(txtPrice.Text, out var price);
+
+            ValidateLength(title, 5);
+            ValidateLength(author, 10);
+            ValidateIsTrue(isPrice, "Inte ett giltigt pris");
+        }
+
+        private void ValidateIsTrue(bool isPrice, string message)
+        {
+            if (isPrice != true)
+                throw new ApplicationException(message);
+        }
+
+        private void ValidateLength(string input, int length)
+        {
+            if (input.Length < length)
+                throw new ApplicationException("För kort.");
+        }
+    }
+}
